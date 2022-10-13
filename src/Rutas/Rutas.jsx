e3292856f48d21907b303558229dbbe5b4023a7e
Toxic Symphony <brayan_stiv96@hotmail.com>
2022-10-13 17:33:22 -0500

    se agrega el componente vista portafolio

--- src/Rutas/Rutas.jsx
+++ src/Rutas/Rutas.jsx
@@ -4,6 +4,8 @@ import { Principal } from "../Principal/Principal.jsx";
 import { SobreMi } from "../SobreMi/SobreMi.jsx";
 import { CasaAlicante } from "../CasaAlicante/CasaAlicante.jsx";
 import { CircuitoMuseoGrafico } from "../CircuitoMuseoGrafico/CircuitoMuseoGrafico.jsx";
+import { VistaPortafolio } from "../VistaPortafolio/VistaPortafolio.jsx";
+
 
 
 
@@ -13,7 +15,7 @@ export function Rutas (){
         <div className="App">
           <Menu></Menu>
           <Routes>
-          {/* <Route path="/inicio" element={<PielesArquitectonicas />} /> */}
+          <Route path="/inicio" element={<VistaPortafolio />} />
             <Route path="/circuito" element={<CircuitoMuseoGrafico />} />
             <Route path="/sobremi" element={<SobreMi />} />
             <Route path="/casaalicante" element={<CasaAlicante />} />

9922c6f2083d74c6a3888cca944ff5a32491d248
403 <403@academicos.cesde.edu.co>
2022-10-14 11:09:05 -0500

    se arregla un error en el texto del componente sobre mi

--- src/SobreMi/SobreMi.jsx
+++ src/SobreMi/SobreMi.jsx
@@ -4,14 +4,14 @@ export function SobreMi(){
     return(
         <>
         <div className="cualquiera">
-            <div class="container-fluid  dark position-absolute end-0  contenedor2 z-index">
-        <div class="row row-cols-2 row-md-2  dark justify-content-center">
-            <div class="col-12 col-md-6">
+            <div className="container-fluid  dark position-absolute end-0  contenedor2 z-index">
+        <div className="row row-cols-2 row-md-2  dark justify-content-center">
+            <div className="col-12 col-md-6">
                 <img src="https://firebasestorage.googleapis.com/v0/b/fotosanotojobs.appspot.com/o/retratos.jpg?alt=media&token=1f1d5143-1644-475a-958d-b2867cf4583a" alt="" class="img-fluid w-100 "/>
             </div>
-                <div class="col-12 col-md-6  text-light  text-center mt-5 dark position-top ">
-                    <p class="fs-1 ">Hola, soy <span class="text-yellow">OSCAR CANO</span> arquitecto y fotografo</p>
-                    <p class="fs-2 ">
+                <div className="col-12 col-md-6  text-light  text-center mt-5 dark position-top ">
+                    <p className="fs-1 ">Hola, soy <span className="text-amarillo fs-1">OSCAR CANO</span> arquitecto y fotografo</p>
+                    <p className="fs-2 ">
                       ... Arquitecto interesado en el desarrollo del conocimiento y
                       experimentación dela Arquitectura, basado en competencias y
                       habilidades en Diseño y Representación gráfica, que busca en la
@@ -19,29 +19,29 @@ export function SobreMi(){
                       el crecimiento personal y profesional
                     </p>
                     
-                    <div class="row d-flex justify-content-around mt-5 dark">
-                      <div class="col-6 text-center">
-                        <p class="fs-6">
+                    <div className="row d-flex justify-content-around mt-5 dark">
+                      <div className="col-6 text-center">
+                        <p className="fs-6">
                           ARQUITECTO
                         </p>
         
                       </div>
-                      <div class="col-6 text-center">
-                        <p class="fs-6">
+                      <div className="col-6 text-center">
+                        <p className="fs-6">
                           FOTOGRAFO
                         </p>
         
                       </div>
         
-                      <div class="row d-flex justify-content-around">
-                        <div class="col-6 col-md-6 text-center ">
-                          <p class="fs-6">
+                      <div className="row d-flex justify-content-around">
+                        <div className="col-6 col-md-6 text-center ">
+                          <p className="fs-6">
                             Universidad Pontificia Bolivariana
                           </p>
           
                         </div>
-                        <div class="col-6 col-md-6 text-center ">
-                          <p class="fs-6">
+                        <div className="col-6 col-md-6 text-center ">
+                          <p className="fs-6">
                             CESDE
                           </p>
                         </div>
